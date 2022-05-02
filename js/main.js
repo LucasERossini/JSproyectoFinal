c77f5c4785502781a3d6e3ab76b067d6e87b2f39
Lucas Ezequiel Rossini <rossiniluca25@gmail.com>
2022-05-02 20:56:44 -0300

    implementando AJAX

--- js/main.js
+++ js/main.js
@@ -1,108 +1,167 @@
 //! Variables
-const acumulador = [[], 0];
-let listaTotal;
-const productos = [];
-let elemento = document.getElementById("compra");
-let add1 = document.getElementById('add1');
-let add2 = document.getElementById('add2');
-let add3 = document.getElementById('add3');
-let add4 = document.getElementById('add4');
+let acumulador = [[], 0, 0]; // [lista de productos agregados, importe total, cantidad total]
+const productos = []; // arreglo de objetos 'Producto'
+let filas = []; // arreglo para crear filas en tabla de HTML
+let compra = document.getElementById("compra");
+const add = [
+    document.getElementById('add1'),
+    document.getElementById('add2'),
+    document.getElementById('add3'),
+    document.getElementById('add4')
+];
 let fin = document.getElementById('fin');
 let reset = document.getElementById('reset');
+let tabla = document.getElementById('tabla');
+let total = document.createElement('p');
+let final = document.getElementById('final');
 
-//! Clase para agrupar características de un prodcto
-class Producto {
-    constructor (nombre, precio, cantidad) {
-        this.nombre = nombre;
-        this.precio = precio;
-        this.cantidad = cantidad;
-    };
-    addCantidad (){
-        this.cantidad ++;
-    };
+// trayendo datos externos
+async function arreglar() {
+    const resp = await fetch('./js/datos.json');
+    const data = await resp.json();
+    data.forEach(producto => {
+        productos.push(producto)
+    });
+    console.log(productos);
+};
+arreglar();
+
+//! Agregar un elemento (agrega los datos de un elemento al arreglo 'acumulador')
+function agregar(i, [a, b, c]) {
+    productos[i].cantidad++;
+    sessionStorage.setItem('producto'+(i+1), productos[i].cantidad);
+    a[i] = (`${productos[i].nombre}`);
+    b += productos[i].precio;
+    c++;
+    return acumulador = [a, b, c];
 };
 
-//! Arreglo para agrupar productos
-productos.push(new Producto("Producto 1: $10", 10, 0));
-productos.push(new Producto("Producto 2: $20", 20, 0));
-productos.push(new Producto("Producto 3: $30", 30, 0));
-productos.push(new Producto("Producto 4: $40", 40, 0));
+//! Agregar un elemento al HTML (muestra en el HTML cada elemento agregado)
+function click(agregando) {
+    for (let index = 0; index < add.length; index++) {
+        switch (agregando) {
+            case add[index]:
+                agregar(index,acumulador);
+            break;
+        };
+    };
+        
+    for (let index = 0; index < acumulador[0].length; index++) {
+        if (acumulador[0][index] == undefined) {
+            continue;
+        } else {
+            if (productos[index].agregado == true) {
+                productos[index].agregado = false;
+                filas[index] = document.createElement('tr');
+                filas[index].innerHTML = `<td>${acumulador[0][index]}</td>
+                                          <td>${productos[index].cantidad}</td>
+                                          <td>${productos[index].precio * productos[index].cantidad}</td>`;
+                tabla.append(filas[index]);
+            } else {
+                filas[index].innerHTML = `<td>${acumulador[0][index]}</td>
+                                          <td>${productos[index].cantidad}</td>
+                                          <td>${productos[index].precio * productos[index].cantidad}</td>`;
+                tabla.append(filas[index]);
+            };
+        };  
+    };
+    total.innerHTML = `El importe total final es de <b>$${acumulador[1]}</b>.`;
+    compra.append(total);
+}; 
 
 //! Agregando productos a almacenamiento local
 const guardarLocal = (clave, valor) => {localStorage.setItem(clave, valor)};
 guardarLocal('listaProductos', JSON.stringify(productos));
 
-//! Agregar elementos a lista
-function click(add) {
-    switch (add) {
-        case add1:
-            productos[0].addCantidad();
-            sessionStorage.setItem('producto1', productos[0].cantidad);
-            acumulador[0][0] = (`${productos[0].cantidad} ${productos[0].nombre}`);
-            acumulador[1] = acumulador[1] + productos[0].precio;
-            break;
-        case add2:
-            productos[1].addCantidad();
-            sessionStorage.setItem('producto2', productos[1].cantidad);
-            acumulador[0][1] = (`${productos[1].cantidad} ${productos[1].nombre}`);
-            acumulador[1] = acumulador[1] + productos[1].precio;
-            break;
-        case add3:
-            productos[2].addCantidad();
-            sessionStorage.setItem('producto3', productos[2].cantidad);
-            acumulador[0][2] = (`${productos[2].cantidad} ${productos[2].nombre}`);
-            acumulador[1] = acumulador[1] + productos[2].precio;
-            break;
-        case add4:
-            productos[3].addCantidad();
-            sessionStorage.setItem('producto4', productos[3].cantidad);
-            acumulador[0][3] = (`${productos[3].cantidad} ${productos[3].nombre}`);
-            acumulador[1] = acumulador[1] + productos[3].precio;
-            break;
-        default:
-            break;
-    };
-    console.log(acumulador[0]);
-    console.log(acumulador[1]);
-    listaTotal = "";
-    for (let index = 0; index < acumulador[0].length; index++) {
-        if (acumulador[0][index] == undefined) {
-            continue;
-        } else {
-            listaTotal += acumulador[0][index] + "\n";
-        };        
-    };
-    console.log(listaTotal);
-    elemento.innerText = `Los productos agregados hasta ahora son:
-                          ${listaTotal}
-                          Y el total hasta ahora es de $${acumulador[1]}.`;
+//! LLamando a la función 'click'
+for (let index = 0; index < add.length; index++) {
+    add[index].onclick = () => {click(add[index])};
 };
 
-add1.onclick = () => {click(add1)};
-add2.onclick = () => {click(add2)};
-add3.onclick = () => {click(add3)};
-add4.onclick = () => {click(add4)};
-
-//! Mostrar y cargar elementos finales agregados
+//! Cargar elementos finales agregados
 fin.onclick = () => {
     if (acumulador[1] == 0) {
-        elemento.innerText = 'No eligió ningún producto.'; // contenido agregado a nueva elemento del HTML
+        Swal.fire({
+            title: 'Error',
+            text: 'No eligió ningún producto',
+            icon: 'error',
+            iconColor: 'red',
+            confirmButtonText: 'Ok'
+        });
     } else {
-        compraFinal = JSON.stringify(acumulador)
-        sessionStorage.setItem('compraFinal', compraFinal);
-        elemento.innerText = `La lista final de productos es:
-                              ${listaTotal}
-                              Y el total final es de $${acumulador[1]}.`;
+        Swal.fire({
+            title: 'Finalizar compra',
+            text: 'El importe final es de $' + acumulador[1] + '. ¿Está seguro que desea finalizar la compra?',
+            icon: 'question',
+            iconColor: 'blue',
+            showCloseButton: true,
+            showCancelButton: true,
+            confirmButtonText: 'Sí',
+            confirmButtonColor: 'green',
+            cancelButtonText: 'No',
+            cancelButtonColor: 'red'
+        }).then ((response) => {
+            if (response.isConfirmed) {
+                Swal.fire({
+                    title: 'Compra Confirmada',
+                    icon: 'success',
+                    iconColor: 'green',
+                    confirmButtonText: 'Ok'
+                });
+                compraFinal = JSON.stringify(acumulador)
+                sessionStorage.setItem('compraFinal', compraFinal);
+                final.innerText = 'Compra efectuada.';
+            } else {
+                Swal.fire({
+                    title: 'Continúa con la compra',
+                    icon: 'info',
+                    confirmButtonText: 'Ok'
+                });
+            };
+        });
     };
 };
 
 //! Reiniciar/borrar datos
 reset.onclick = () => {
-    acumulador[0] = [];
-    acumulador[1] = 0;
-    for (let index = 0; index < productos.length; index++) {
-        productos[index].cantidad = 0;        
-    };
-    sessionStorage.clear();
-    elemento.innerText = '';
+    Swal.fire({
+        title: 'Reiniciar compra',
+        text: '¿Está seguro que desea reiniciar la compra?',
+        icon: 'question',
+        iconColor: 'blue',
+        showCloseButton: true,
+        showCancelButton: true,
+        confirmButtonText: 'Sí',
+        confirmButtonColor: 'green',
+        cancelButtonText: 'No',
+        cancelButtonColor: 'red'
+    }).then ((response) => {
+        if (response.isConfirmed) {
+            Swal.fire({
+                title: 'Reinicio Confirmado',
+                icon: 'success',
+                iconColor: 'green',
+                confirmButtonText: 'Ok'
+            });
+            acumulador = [[], 0, 0];
+            for (let index = 0; index < productos.length; index++) {
+                productos[index].cantidad = 0;   
+            };
+            for (let index = 0; index < filas.length; index++) {
+                if (filas[index] == undefined) {
+                    continue;
+                } else {
+                  filas[index].innerText = "";  
+                };                
+            };
+            sessionStorage.clear();
+            total.innerHTML = "";
+        } else {
+            Swal.fire({
+                title: 'Reinicio Cancelado',
+                icon: 'info',
+                confirmButtonText: 'Ok'
+            });
+        };
+    });
 };
